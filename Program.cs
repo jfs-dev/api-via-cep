b128113b858ff66c3fbd0fdb3b21f53f76d14521
Jeú <54154628+jfs-dev@users.noreply.github.com>
2022-09-01 13:17:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static void ConsultarCEP()
+{
+    var httpClient = new HttpClient();
+
+    var response = httpClient.GetAsync("https://viacep.com.br/ws/01001000/json/").Result;
+
+    var content = response.Content.ReadAsStringAsync().Result;
+
+    Console.WriteLine(content);
+}
+
+Console.WriteLine("Testando API Via CEP");
+Console.WriteLine("--------------------");
+ConsultarCEP();
+
+Console.ReadKey();
